f99782cec702309f10444cced9b425c6141741f4
Leticia Gonçalves <letigsouza03@gmail.com>
2025-10-17 13:53:44 -0300

    Exercise 1.5: course information — 5º passo

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -25,7 +25,8 @@ const Content = ({ parts }) => {
 }
 
 const Total = ({ parts }) => {
-  const total = parts.reduce((sum, p) => sum + p.exercises, 0)
+  let total = 0
+  parts.forEach(p => total += p.exercises)
   return (
     <p>
       Number of exercises {total}
@@ -34,8 +35,9 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = 'Desenvolvimento de aplicação Half Stack'
-  const parts = [
+  const course = { 
+    name: 'Desenvolvimento de aplicação Half Stack' ,
+    parts: [
     {
       name: 'Fundamentos da biblioteca React',
       exercises: 10
@@ -48,13 +50,13 @@ const App = () => {
       name: 'Estado de um componente',
       exercises: 14
     }
-  ]
+  ]}
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
